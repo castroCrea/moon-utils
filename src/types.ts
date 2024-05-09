2966ff7109b25088bb0abf06495c60c68e29c96d
Castro Crea <castro.crea@gmail.com>
2024-05-09 18:12:56 +0200

    ➖

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,3 @@
-import { type Context } from '@moonjot/moon'
-
 export type LOG = ((log: string) => void) | undefined
 
-export type SearchObject = Context & {
-  title?: string
-  task?: string
-  content?: string
-}
+export type SearchObject = Record<string, any>
