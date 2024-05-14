d003d8e6ad6776802e20a4d1246f6887295c3c8e
Castro Crea <castro.crea@gmail.com>
2024-05-14 16:24:22 +0200

    💚

--- dist/handleAnchors.js
+++ dist/handleAnchors.js
@@ -73,9 +73,9 @@ const handleConditions = ({ content, searchObj }) => {
         const ifValue = (_a = value.match(regexIfStart)) === null || _a === void 0 ? void 0 : _a[0];
         if (!ifValue)
             return;
-        const key = (ifValue.replace('{{IF ', '').replace('}}', '')).toLowerCase();
+        const key = (ifValue.replace('{{IF ', '').replace('}}', ''));
         const comparator = Object.keys(comparatorsSetUp).find(element => key.toLowerCase().includes(element.toLowerCase()));
-        const keyValue = comparator ? (_b = comparatorsSetUp[comparator]) === null || _b === void 0 ? void 0 : _b.callback({ key, searchObj }) : (0, searchObject_1.searchObject)({ obj: searchObj, path: key });
+        const keyValue = comparator ? (_b = comparatorsSetUp[comparator]) === null || _b === void 0 ? void 0 : _b.callback({ key, searchObj }) : (0, searchObject_1.searchObject)({ obj: searchObj, path: key.toLowerCase() });
         if (!keyValue) {
             try {
                 const trimmedValue = value.substring(1).replaceAll(/[.*+?^${}()|[\]\\]/g, '\\$&');
